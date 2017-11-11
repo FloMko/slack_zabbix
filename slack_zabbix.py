6ab9c9cf648637928d89812b20af1757d6dae4e7
Max <flomko.ru@gmail.com>
2017-11-12 00:03:49 +0300

    Update slack_zabbix
    refactoring, new class

--- slack_zabbix.py
+++ slack_zabbix.py
@@ -29,33 +29,31 @@ def cli_args():
 
 def parse_configs(path):
     """Parse configuration YAML configuration file."""
-    with open(path) as cfg:
-        config = yaml.load(cfg.read())
+    try:
+        with open(path) as cfg:
+            config = yaml.load(cfg.read())
+    except FileNotFoundError:
+        path = '/usr/local/etc/zabbix3/zabbix/alertscripts/slack_zabbix.cfg'
+        with open(path) as cfg:
+            config = yaml.load(cfg.read())
     return config
 
 
-def serialize_data(**kwargs):
-    """Format request body to JSON."""
-    data = {
-        'channel': kwargs['channel'],
-        'username': 'zabbixalerbot',
-        'text': kwargs['message'],
-        # 'icon_emoji': kwargs['emoji'],
-    }
-    return json.dumps(data)
-
-
-class Zabbix(object):
+class Zabbix(object,):
     """ Class for iteraction with
     Zabbix Communication."""
-    def __init__(self, config_path=None):
-        config_path = config_path if config_path is not None \
-            else '/usr/local/etc/zabbix3/zabbix/alertscripts/slack_zabbix.cfg'
+    def __init__(self,  config_path = './slack_zabbix.cfg'):
         config = parse_configs(config_path)
         self.auth={}
         self.auth['user'] = config['auth']['user']
         self.auth['password'] = config['auth']['password']
         self.auth['zabbix_url'] = config['auth']['zabbix_url']
+        self.auth['zabbix_api'] = config['auth']['zabbix_api']
+        # header for api requests
+        req_headers = {
+            'Content-Type': 'application/json-rpc'
+        }
+
 
     def get_image(self, zbx_img_url ):
         LOGINURL = self.auth['zabbix_url']
@@ -64,7 +62,6 @@ class Zabbix(object):
         req_headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
-
         # payload for typing form
         formdata = {
             'name': self.auth['user'],
@@ -72,18 +69,12 @@ class Zabbix(object):
             'enter': 'Sign in',
             'form_refresh': '1',
         }
-
         session = requests.session()
         # Authenticate at zabbix frontend
         r = session.post(self.auth['zabbix_url'], data=formdata,
                          headers=req_headers, allow_redirects=False)
-        # Debag info
-
-
         # open php dynamick graph and save
         r2 = session.get(DATAURL)
-    # Debag info
-
     # write to file
         with open('/tmp/zabbix.png', 'wb') as f:
             f.write(r2.content)
@@ -97,7 +88,6 @@ class Zabbix(object):
             "zbx_trigger": None,
             "zbx_itemname": None,
         }
-
         settings_description = {
             "itemid": {"name": "zbx_itemid", "type": "int"},
             "title": {"name": "zbx_title", "type": "str"},
@@ -120,7 +110,6 @@ class Zabbix(object):
 
             else:
                 zbx_body_text.append(line)
-
         # return(settings['zbx_trigger'], settings['zbx_title'])
         zbx_img_url = Zabbix.get_image_id(self, settings['zbx_title'], settings['zbx_itemid'])
         return(zbx_img_url)
@@ -130,7 +119,6 @@ class Zabbix(object):
         """Atraction to zabbix api to know graph id"""
         name = hostname
         # name_item = trigername
-        url = self.auth['zabbix_url'] + 'api_jsonrpc.php'
         item = itemid
         zbx_img_url = 'https://bigbro.m10m.ru/zabbix/chart3.php?period=900&name=' + str(name) \
             + '&width=900&height=300&graphtype=0&legend=1&items[0][itemid]=' \
@@ -138,15 +126,11 @@ class Zabbix(object):
         return(zbx_img_url)
 
 
-
-
-
-
 class Slack(object):
 
     """Class for Slack API"""
 
-    def __init__(self, config_path=None):
+    def __init__(self, config_path='./slack_zabbix.cfg'):
         """
         Construct class.
         Constructor Arguments in kwargs and configuration
@@ -161,27 +145,14 @@ class Slack(object):
         self.emoji['default'] = config['emoji']['default']
         self.slack = {}
         self.slack['token'] = config['slack']['token']
-        # same add for other part
-
-    # def _set_emoji(self, subject):
-    #     """Set emoji to use depending on the subject."""
-    #     if 'RECOVERY' in subject:
-    #         emoji = self.emoji['recovery']
-    #     elif 'PROBLEM' in subject:
-    #         emoji = self.emoji['problem']
-    #     else:
-    #         emoji = self.emoji['default']
-    #     return emoji
 
 
-    def send_event(self, **kwargs):
+    def send_event(self, message):
         """Send request to slack BE."""
-        payload = serialize_data(**kwargs)
-        print('THIS IS ' + payload)
         i = 0
         while i < 6:
             try:
-                data={'token':self.slack['token'], 'channel':'#zabbix', 'text':payload}
+                data={'token':self.slack['token'], 'channel':'#zabbix', 'text':message}
                 resp = requests.post(
                     'https://slack.com/api/chat.postMessage',
                     data=data,
@@ -207,9 +178,6 @@ class Slack(object):
             headers={'Accept': 'application/json'}, files=f)
 
 
-
-
-
 class Attraction(object):
     """basik unit action
     """
@@ -221,28 +189,14 @@ class Attraction(object):
             'body',
             'Zabbix did not provide a message body'
         )
-        # body =(
-        # 'zbx;trigger:Memory Pages Input/sec \n'
-        # 'zbx;itemid:38207\n'
-        # 'zbx;itemname:Windows 2008 R2 Memory Pages Input/sec\n'
-        # 'zbx;title:mfs01.spb.drlan.ru\n'
-        # )
         message = '{0}: {1}'.format(subject, body)
-        Slack().send_event(
-            # emoji=emoji,
-            channel=channel,
-            message=message
-              )
+        Slack().send_event(message)
         zbx_img_url = Zabbix().find_item(body)
         Zabbix().get_image(zbx_img_url)
         Slack().post_image()
 
 
-
-
-
-
-
 if __name__ == '__main__':
     ARGS = cli_args()
     Attraction().broadcast(**ARGS)
+
